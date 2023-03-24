8b8fd30d6212c8c1e832fccb40da565015dd11b1
SeLeZniOFF <mrilison11@gmail.com>
2023-03-25 00:19:10 +0300

    рабочая версия с кнопками просмотра

--- bot.py
+++ bot.py
@@ -10,64 +10,65 @@ load_dotenv()
 bot = telebot.TeleBot(os.getenv("BOT_TOKEN"))
 
 # Define the categories
-categories = ["1.Для чата стрингеров", "2.Для поддержки 360", "3.Для чата Терминал доработки", "4.МА ПодмСегодня муниципалы"]
+categories = ["1.Для чата стрингеров", "2.Для поддержки 360", "3.Для чата Терминал доработки", "4.МА ПодмСегодня муниципалы", "5.Тезисы к созвонам"]
+
+def create_main_menu():
+    markup = telebot.types.InlineKeyboardMarkup()
+    for category in categories:
+        save_button = telebot.types.InlineKeyboardButton("💾 " + category.split(".")[1], callback_data="save_" + category)
+        view_button = telebot.types.InlineKeyboardButton("👁 " + category.split(".")[1], callback_data="view_" + category)
+        markup.row(save_button, view_button)
+    return markup
 
 # Handler for /start command
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    markup = telebot.types.InlineKeyboardMarkup()
-    for category in categories:
-        button = telebot.types.InlineKeyboardButton(category.split(".")[1], callback_data=category)
-        markup.add(button)
-    bot.reply_to(message, "Привет! Этот бот поможет тебе сохранять заметки в разных категориях. Выбери категорию, в которую хочешь добавить заметку:", reply_markup=markup)
+    markup = create_main_menu()
+    bot.reply_to(message, "Привет! Этот бот поможет тебе сохранять заметки в разных категориях. Выбери категорию, в которую хочешь добавить заметку или просмотреть заметки:", reply_markup=markup)
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    markup = create_main_menu()
+    bot.reply_to(message, "Для того чтобы добавить заметку, выберите категорию и напиши сообщение. \n"
+    "Для того чтобы просмотреть готовое сообщение выберите кнопку с глазом напротив категории.\n",
+    reply_markup=markup)
 
 # Handler for inline keyboard button presses
 @bot.callback_query_handler(func=lambda call: True)
-def save_note_callback(call):
-    categories_to_save = [call.data]
+def handle_callback(call):
+    action, category = call.data.split("_", 1)
+    if action == "save":
+        save_note_callback(call, category)
+    elif action == "view":
+        view_notes_callback(call, category)
+
+def save_note_callback(call, category):
     message = call.message
-    bot.answer_callback_query(call.id, "Заметка будет сохранена в категории {}".format(call.data.split(".")[1]))
+    bot.answer_callback_query(call.id, "Заметка будет сохранена в категории {}".format(category.split(".")[1]))
     bot.edit_message_text("Отправь сообщение, которое нужно сохранить:", message.chat.id, message.message_id)
-    bot.register_next_step_handler(message, save_note_multiple_categories, categories_to_save)
-@bot.message_handler(commands=['view'])
-def view_file(message):
+    bot.register_next_step_handler(message, save_note, category)
+
+def view_notes_callback(call, category):
+    message = call.message
+    filename = category.split(".")[1].replace(" ", "_") + ".md"
+    filepath = os.path.join(os.getcwd(), filename)
     try:
-        # Get the file name from the command arguments
-        file_name = message.text.split()[1]
-        # Add the .md extension if it's not already included
-        if not file_name.endswith('.md'):
-            file_name += '.md'
-        # Open the file and read its contents
-        with open(file_name, "r", encoding="utf-8") as f:
+        with open(filepath, "r", encoding="utf-8") as f:
             file_content = f.read()
-        # Send the file's contents to the user as a message
-        bot.reply_to(message, file_content)
-    except IndexError:
-        # If no file name is specified, display an error message
-        bot.reply_to(message, "Ошибка. Не указано название файла.")
+        bot.answer_callback_query(call.id)
+        bot.send_message(message.chat.id, file_content)
     except FileNotFoundError:
-        # If the file is not found, display an error message
-        bot.reply_to(message, "Ошибка. Файл не найден.")
+        bot.answer_callback_query(call.id, "Ошибка. Файл не найден.")
 
-
-# Handler for all other messages
-def save_note_multiple_categories(message, categories_to_save):
+def save_note(message, category):
     clean_message = re.sub(r'\\d+', '', message.text).strip()
-    saved_categories = []
-    for category in categories_to_save:
-        filename = category.split(".")[1].replace(" ", "_") + ".md"
-        filepath = os.path.join(os.getcwd(), filename)
-        with open(filepath, "a", encoding="utf-8") as f:
-            f.write("📌 " + clean_message + "\n\n")
-        saved_categories.append(category.split(".")[1])
-    categories_str = ', '.join(saved_categories)
-    bot.reply_to(message, f"Заметка сохранена в категориях: {categories_str}")
-    markup = telebot.types.InlineKeyboardMarkup()
-    for category in categories:
-        button = telebot.types.InlineKeyboardButton(category.split(".")[1], callback_data=category)
-        markup.add(button)
-    bot.send_message(message.chat.id, "Выбери категорию, в которую хочешь добавить заметку:", reply_markup=markup)
-
+    filename = category.split(".")[1].replace(" ", "_") + ".md"
+    filepath = os.path.join(os.getcwd(), filename)
+    with open(filepath, "a", encoding="utf-8") as f:
+        f.write("📌 " + clean_message + "\n\n")
+    bot.reply_to(message, f"Заметка сохранена в категории: {category.split('.')[1]}")
+    markup = create_main_menu()
+    bot.send_message(message.chat.id, "Выбери категорию, в которую хочешь добавить заметку или просмотреть заметки:", reply_markup=markup)
 
 # Start the bot
 bot.polling()
